ed28972ecf332d01da782907e0643461fa2f30e2
FrancoArielTorres <fatorres92.fat@gmail.com>
2023-04-15 01:06:05 -0300

    Primer commit

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+const edad = prompt("ingrese su edad");
+if (edad < 18) {
+  alert("Sos menor de edad, entras bajo tu responsabilidad.");
+} else {
+  alert("Podes entrar, sos mayor de edad.");
+}
+var opcion = prompt ("Se encuentra en San Nicolas?")
+if (opcion=="si") {
+  alert   ("El envio no tiene costo")
+}
+else{
+  alert ("El envio tiene un costo desde $1000 ")
+}
